ad754b832a58271bff6e291d1aa9aadc1fc5c32e
Gulieva Marina <108300153+GuliMari@users.noreply.github.com>
2022-12-05 00:42:25 +0300

    Update watchlog-monitor.sh

--- watchlog-monitor.sh
+++ watchlog-monitor.sh
@@ -5,6 +5,5 @@ cp /home/vagrant/files/watchlog.sh /opt/watchlog.sh
 cp /home/vagrant/files/watchlog.service /etc/systemd/system/watchlog.service
 cp /home/vagrant/files/watchlog.timer /etc/systemd/system/watchlog.timer
 echo "ALERT" > /var/log/watchlog.log
-systemctl daemon-reload
 systemctl enable --now watchlog.service
 systemctl enable --now watchlog.timer
